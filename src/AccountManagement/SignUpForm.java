25d5b42ed779fcd1474579555c9fc7f55949d78a
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2024-12-12 23:57:37 +0200

    ربنا يعينا

--- src/AccountManagement/SignUpForm.java
+++ src/AccountManagement/SignUpForm.java
@@ -13,19 +13,42 @@ public class SignUpForm {
     private JRadioButton lecturerRadioButton;
     private JRadioButton adminRadioButton;
     private JButton signUpButton;
+    private JFrame frame;
 
     public SignUpForm() {
-        studentRadioButton.addActionListener(e -> {
-            if (studentRadioButton.isSelected()) {
-                lecturerRadioButton.setSelected(false);
-                adminRadioButton.setSelected(false);
-                try {
-                    SignUp.addUser(textField1.getText(), passwordField1.getText(), "Student");
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    JOptionPane.showMessageDialog(contentPane, "Error adding user: " + ex.getMessage());
+        frame = new JFrame("SignUpForm");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (studentRadioButton.isSelected()) {
+                    try {
+                        SignUp.addUser(textField1.getText(), passwordField1.getText(), "Student");
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(contentPane, "Error adding user: " + ex.getMessage());
+                    }
+                } else if (lecturerRadioButton.isSelected()) {
+                    try {
+                        SignUp.addUser(textField1.getText(), passwordField1.getText(), "Lecturer");
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(contentPane, "Error adding user: " + ex.getMessage());
+                    }
+                } else if (adminRadioButton.isSelected()) {
+                    try {
+                        SignUp.addUser(textField1.getText(), passwordField1.getText(), "Admin");
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(contentPane, "Error adding user: " + ex.getMessage());
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(contentPane, "Please select a role");
                 }
             }
         });
+    }
+    public void show() {
+        frame.setVisible(true);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import AccountManagement.SignUp;
+import AccountManagement.SignUpForm;
 import DataStorage.FileHandler;
 import UserTypes.*;
+
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -9,6 +12,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         new FileHandler().readSavedUsersFromFile(new File(Student.studentInformation), Student.students);
-        new SignUp().addUser("Gamal", "password", "Student");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SignUpForm().show();
+            }
+        });
     }
 }
