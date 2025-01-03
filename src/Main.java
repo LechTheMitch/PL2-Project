fae6b102adb1447c3e61ccbfa728900f0f0d1d39
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2025-01-03 14:25:13 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import AccountManagement.LoginForm;
 import DataStorage.FileHandler;
 import UserTypes.*;
 
@@ -8,17 +7,6 @@ import java.io.IOException;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws IOException {
-        loadUsers();
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new LoginForm().setVisible(true);
-            }
-        });
-    }
-    private static void loadUsers(){
-        FileHandler.readSavedUsersFromFile(Student.studentInformation, Student.students, Student.class);
-        FileHandler.readSavedUsersFromFile(Admin.adminInformation, Admin.admins, Admin.class);
-        FileHandler.readSavedUsersFromFile(Lecturer.lecturerInformation, Lecturer.lecturers, Lecturer.class);
+        FileHandler.loadUsers();
     }
 }
